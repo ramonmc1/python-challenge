a447b1ada8b67e14b8e2cfda72527a534f3dcab3
ramon <ramon@MacBook-Pro.local>
2022-01-01 23:01:38 -0500

    final update

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,29 +6,29 @@ csv_path = os.path.join("Resources", "budget_data.csv")
 with open(csv_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csvheader = next(csvreader)
-    total_count = 0
+    total_count = 0 #setting initial counts for variables
     total_net = 0
     result = 0
     change = 0
     averagechange = 0
-    changemax = change
-    changemin = change
+    changemax = 0
+    changemin = 0
 
-    for row in csvreader:
+    for row in csvreader: #for loop to get the total count and to add the monthly revenue
         total_count += 1
         total_net += int(row[1])
-        last_result = result
-        result = int(row[1])
-        changelast = change
+        last_result = result # capture previous value to calculate change (i-1)
+        result = int(row[1]) # set up new value (i=1)
+
         if total_count!=1: #since there is not data before Jan-10, the initial change is omitted
             change = result - last_result
-            averagechange = (averagechange + change)
+            averagechange = (averagechange + change) #add average change to calculate the 'average change'
             total_average_change = averagechange / (total_count - 1)
         
-        if (change >= changemax):
+        if (change >= changemax): #this boolean will only save the highest change as the loop goes through the list
                 changemax = change
                 maxperiod = row[0]
-        if (change <= changemin):
+        if (change <= changemin): #saves the min change as the loop goes through the list
                 changemin = change
                 minperiod = row[0]
         
@@ -42,15 +42,11 @@ with open(csv_path) as csvfile:
     print ("Greatest Decrease in Profits: " + str(minperiod) + " " + str(changemin))
     print ("```````")
 
-with open('analysis/financial_analysis.txt', 'w') as txtf:
+with open('analysis/financial_analysis.txt', 'w') as txtf: #create and export txt file to analysis folder
     txtf.write("Financial Analysis"'\n')
-    txtf.write("------------------------------------------"'\n') 
-    txtf.write("Total Months: " + str(total_count)) 
-    txtf.write('\n')
-    txtf.write("Total Revenue: $" + str(total_net))
-    txtf.write('\n')
-    txtf.write("Average Change: $" + str(round(total_average_change)))
-    txtf.write('\n')
-    txtf.write("Greatest Increase in Profits: " + str(maxperiod) + " " + str(changemax))
-    txtf.write('\n')
-    txtf.write("Greatest Decrease in Profits: " + str(minperiod) + " " + str(changemin))
+    txtf.write("------------------------------------------") 
+    txtf.write('\n'"Total Months: " + str(total_count)) 
+    txtf.write('\n'"Total Revenue: $" + str(total_net))
+    txtf.write('\n'"Average Change: $" + str(round(total_average_change)))
+    txtf.write('\n'"Greatest Increase in Profits: " + str(maxperiod) + " " + str(changemax))
+    txtf.write('\n'"Greatest Decrease in Profits: " + str(minperiod) + " " + str(changemin))
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,19 +3,17 @@ import csv
 
 csv_path = os.path.join("Resources", "election_data.csv")
 
-
-with open(csv_path) as csvfile:
+with open(csv_path) as csvfile: #open the csv file
     csvreader = csv.reader(csvfile, delimiter=",")
-    csvheader = next(csvreader)
-    total_count = 0
-    candidate_count = [0, 0, 0, 0] 
-    candidates = ['Khan', 'Correy', 'Li', "O'Tooley"]
-    
+    csvheader = next(csvreader) # csv header is skipped
+        
     #I used the code below first to identify all the canidates, including any "write in" candidates
     #for row in csvreader:
-     #   total_count += 1
-      #  if row[2] not in candidates: 
-       #    candidates.append(row[2])
+    #   if row[2] not in candidates: 
+    #      candidates.append(row[2])
+    candidates = ['Khan', 'Correy', 'Li', "O'Tooley"]
+    candidate_count = [0, 0, 0, 0] #setup initial count as a list
+    total_count = 0 
 
     # Going through each row to count the votes and assign them to a list called "candidate_count"
     for row in csvreader:
@@ -29,36 +27,35 @@ with open(csv_path) as csvfile:
         elif row[2] == candidates[3]:
             candidate_count[3] +=1
 
-   #created a list with percent win (results)
+    #created a list with percent win (results)
     highercount = 0
     results = []
     for i in range(4):
-       percent_win = (round((100 * (candidate_count[i]/ total_count)), 3))
-       if candidate_count[i] >= highercount:
+        percent_win = (round((100 * (candidate_count[i]/ total_count)), 3)) #round percent to 3 decimal places
+        if candidate_count[i] >= highercount: #boolean to identify the candidate with more votes
             highercount = candidate_count[i]
             winner = candidates[i]
-       results.append(percent_win)
-        
+        results.append(percent_win) # create a list with the results for each candidate 
     print ("```````")
     print ("Election Results") 
     print ("------------------------------------------") 
     print ("Total Votes: " + str(total_count))  
     print ("------------------------------------------")  
-    for i in range(4):
-        print(f'{candidates[i]}: {results[i]}% ({candidate_count[i]})') 
+    for i in range(4): #loop to print the results for each candidade
+       print(f'{candidates[i]}: {results[i]}% ({candidate_count[i]})') 
     print ("------------------------------------------")
     print("Winner: " + winner)
     print ("------------------------------------------")
     print ("```````")
 
-with open('analysis/poll_results.txt', 'w') as txtf:
+with open('analysis/poll_results.txt', 'w') as txtf: #create and write a txt file in analysis folder
     txtf.write("Election Results"'\n')
     txtf.write("------------------------------------------"'\n') 
     txtf.write("Total Votes: " + str(total_count))  
     txtf.write('\n')
     txtf.write("------------------------------------------"'\n')  
-    for i in range(4):
-        txtf.writelines(f' {candidates[i]}: {results[i]}% ({candidate_count[i]})''\n')
+    for i in range(4): #use a loop to create the results candidate - percent votes - raw count
+       txtf.writelines(f' {candidates[i]}: {results[i]}% ({candidate_count[i]})''\n')
     txtf.write("------------------------------------------"'\n') 
     txtf.writelines(f'Winner: {winner}''\n')
     txtf.write("------------------------------------------"'\n') 
